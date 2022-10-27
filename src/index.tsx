337d99c26b878e272d1b12b63b2b7e31dc253195
CMJ <chemingjun@kuaishou.com>
2022-10-27 10:26:02 +0800

    add package info

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import Main from "./Main";
+import pkg from '../package.json'
+
+console.log(`%c ${pkg.name} %c@${pkg.version}`,`color:#FFF;background:#fa6400`,``,``)
 
 export function render(json, opts?: { env?, comDefs?,observable?,ref? } = {}) {
   return (
