c00372cc65d623622fa6308aaac55cb9b03c210b
liuzhigang06 <liuzhigang06@kuaishou.com>
2023-05-09 14:17:32 +0800

    test: add test case

---
+++ cypress/test-case/merge-data-in-click/test.cy.ts
@@ -0,0 +1,13 @@
+import renderTestPage from "../utils"
+import json from './json.json'
+
+describe('点击后触发数据合并', () => {
+  it('点击后触发数据合并', () => {
+    // it函数的回调不能是异步函数，异步函数只能放在cy.then里面
+      const page = renderTestPage(json)
+      cy.mount(page)
+      cy.contains('没有合并数据')
+      cy.contains('按钮').click()
+      cy.contains('收到数据合并')
+  })
+})
