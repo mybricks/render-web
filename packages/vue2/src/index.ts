2929c34d8f729ee109be4c866c075b288a37736a
leon-js <42924541+leon-js@users.noreply.github.com>
2023-10-13 12:01:49 +0800

    feat:vue2版本入口与render-web对其

--- packages/vue2/src/index.ts
+++ packages/vue2/src/index.ts
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-export function render({json, opts}, selector) {
+export function render(props) {
+  return Vue.extend({
+    render: h => h(App, { props })
+  })
+}
+
+window['mybricks-render-vue2'] = function render2({json, opts}, selector) {
   new Vue({
     render: (h) => h(App, { props: { json, opts }}, []),
   }).$mount(selector);
