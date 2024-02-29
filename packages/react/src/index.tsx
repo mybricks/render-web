f2b65462457a01c39b3dcf79bfdbe6bf62e98725
leon-js <42924541+leon-js@users.noreply.github.com>
2024-02-29 21:48:02 +0800

    fix

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -376,7 +376,7 @@ export function useMyBricksRenderContext () {
   return context
 }
 
-import { transformToJSON } from "../../utils/src"
+import { transformToJSON, transformSingleToJSON } from "../../utils/src"
 
 export { transformToJSON }
 
@@ -384,9 +384,9 @@ export function render(toJson: ToJSON | MultiSceneToJSON, options: RenderOptions
   let json = toJson
 
   // 调试或搭建态需要处理
-  // if (options.env.edit || options.env.runtime?.debug) {
-  //   json = JSON.parse(JSON.stringify(toJson))
-  // }
+  if (options.env.edit || options.env.runtime?.debug) {
+    json = JSON.parse(JSON.stringify(toJson))
+  }
   if (!json) {
     return null
   } else {
@@ -396,14 +396,14 @@ export function render(toJson: ToJSON | MultiSceneToJSON, options: RenderOptions
         options.env = deepCopy(options.env)
         // TODO：需不需要把runtime.debug删了，这里弹窗是这样判断是否在调试环境的
       }
-      // transformToJSON(json);
+      transformToJSON(json);
       jsx = <MultiScene json={json} options={options}/>
     } else {
       if (json.slot) {
         // 检查一下这个json.type的判断能否去掉
-        // if (options.env.edit && json.type === 'module') {
-        //   transformSingleToJSON(json)
-        // }
+        if (options.env.edit && json.type === 'module') {
+          transformSingleToJSON(json)
+        }
         jsx = <Main json={json} options={options} root={json.type === 'module' ? false : true}/>
       }
     }
