33f548353e2f89ced6bf6b54fcc3cc1726e67601
leon-js <42924541+leon-js@users.noreply.github.com>
2024-05-20 10:18:38 +0800

    chore:[render-core]

--- packages/core/scripts/build.ts
+++ packages/core/scripts/build.ts
@@ -4,10 +4,14 @@ import ts from "gulp-typescript";
 const cwd = process.cwd();
 
 const tsConfig: ts.Settings = {
-  module: 'es6', // esm commonjs
-  target: 'es6', // 目标
+  module: "es6", // esm commonjs
+  target: "es6", // 目标
   declaration: true, // 自动生成.d.ts
   skipLibCheck: true, // 跳过库文件检查
+
+
+  // allowSyntheticDefaultImports: true,
+  // moduleResolution: "node"
 }
 
 // TypeScript编译任务
@@ -15,7 +19,6 @@ function compileTs() {
   const tsResult = gulp.src(["**/*.ts", "!scripts/**"], {
     base: cwd
   }).pipe(ts(tsConfig))
-
   const dest = gulp.dest('dist/es');
 
   tsResult.js.pipe(dest);
