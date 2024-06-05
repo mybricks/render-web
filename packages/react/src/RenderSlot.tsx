b0588492de33fec56afb9f30034b82b0b4775da4
leon-js <42924541+leon-js@users.noreply.github.com>
2024-06-05 18:01:37 +0800

    fix(react渲染器):智能布局仅使用算法生成的样式信息

--- packages/react/src/RenderSlot.tsx
+++ packages/react/src/RenderSlot.tsx
@@ -88,7 +88,7 @@ export default function RenderSlot({
     const slotStyle = style
     // 智能布局下，默认flex布局，方向为column
     return (
-      <div data-isslot='1' className={`${calSlotClasses(slotStyle)}${root && className ? ` ${className}` : ''}`} style={{ overflow: root ? (showType === "module" ? "hidden" : "hidden auto") : null, ...calSlotStyles(slotStyle, true, root, slot.type === "module", options), ...propsStyle, display: 'flex', flexDirection: "column"}}>
+      <div data-isslot='1' className={`${calSlotClasses(slotStyle)}${root && className ? ` ${className}` : ''}`} style={{ overflow: root ? (showType === "module" ? "hidden" : "hidden auto") : null, ...calSlotStyles(slotStyle, false, root, slot.type === "module", options), ...propsStyle, display: 'flex', flexDirection: "column"}}>
         {layoutTemplate.map((rstTraverseElement: any, index: any) => {
           return renderRstTraverseCom2({com: rstTraverseElement, index, env, getComDef, context, scope, inputs, outputs, _inputs, _outputs, _env, template, onError, logger, createPortal, options})
         })}
